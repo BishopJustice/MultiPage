cea48bb794d3550b938cb154d29346d743e66383
bishopjustice <luke@lyft.com>
2016-08-31 13:53:12 -0700

    Added datatables for sortable tables

--- app/static/functions.js
+++ app/static/functions.js
@@ -2,14 +2,6 @@ function toggler(id){
     $(id).toggle()
 }
 
-function openall() {
-    var input = someList
-    
-    for (each in someList) {
-        if (someList[each].substring(0, 8) != "https://" && someList[each].substring(0, 7) != "http://") {
-            someList[each] = "http://" + someList[each];
-        }  
-        console.log(someList[each])
-        window.open(someList[each]);
-    }
-}
+$(document).ready(function() {
+                $('#dtable').DataTable();
+            } );
